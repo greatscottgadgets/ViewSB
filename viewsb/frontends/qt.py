d35441ca23d58073d2cbaf40b48ef53be63127cb
Mikaela Szekely <mikaela.szekely@qyriad.me>
2020-03-31 17:29:40 -0600

    qt: miscellaneous fixes and cleanup
    In particular, it no longer `raise`s if a packet doesn't have any detail
    fields to show, and no longer tries to cast packets directly to strings.

--- viewsb/frontends/qt.py
+++ viewsb/frontends/qt.py
@@ -32,7 +32,7 @@ def stringify_list(l: []) -> [str]:
 
 def get_packet_string_array(viewsb_packet):
     """ Tiny helper to return and stringify the common fields used for the columns of tree items. """
-    return stringify_list([viewsb_packet.timestamp, viewsb_packet.summarize(), viewsb_packet.summarize_status(), viewsb_packet.summarize_data()] + [viewsb_packet])
+    return stringify_list([viewsb_packet.timestamp, viewsb_packet.summarize(), viewsb_packet.summarize_status(), viewsb_packet.summarize_data()]) + [viewsb_packet]
 
 
 def recursive_packet_walk(viewsb_packet, packet_children_list):
@@ -75,106 +75,26 @@ class QtFrontend(ViewSBFrontend):
         return None
 
 
-    def __init__(self):
-        """ Sets up the Qt UI. """
-
-        QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
-
-        self.app = QApplication([])
-        self.ui_file = QtCore.QFile(os.path.dirname(os.path.realpath(__file__)) + '/qt.ui')
-
-        self.loader = QUiLoader()
-        self.window: QtWidgets.QMainWindow = self.loader.load(self.ui_file)
-
-        # The default column size of 100 is too small for the summary column.
-        self.window.usb_tree_widget.setColumnWidth(1, 400)
-
-        self.window.update_timer = QtCore.QTimer()
-        self.window.update_timer.timeout.connect(self.update)
-
-        self.window.usb_tree_widget.currentItemChanged.connect(self.tree_current_item_changed)
-
-        self.window.usb_tree_widget: QtWidgets.QTreeWidget = self.window.usb_tree_widget
-        self.window.usb_tree_widget.sortByColumn(0)
-
-
-        self.window.showMaximized()
-
-
-    def update(self):
-        """ Called by the QTimer `update_timer`, collects packets waiting the queue and adds them to the tree view.
-
-        Note: Since this is called via a QTimer signal, this method runs in the UI thread.
-        """
-
-        packet_list = []
-
-        try:
-            # Get as many packets as we can as quick as we can.
-            while(True):
-
-                packet = self.data_queue.get_nowait()
-                packet_list.append(packet)
-
-        # But the instant it's empty, don't wait for any more; just send them to be processed.
-        except multiprocessing.queues.Empty:
-            pass
-
-        finally:
-            # In case the queue was empty in the first place and didn't have anything ready.
-            if len(packet_list) > 0:
-
-                self.add_packets(packet_list)
-
-
-    def add_packets(self, viewsb_packets: []):
-        """ Adds a list of top-level ViewSB packets to the tree.
-
-        We're in the UI thread; every bit of overhead counts, so let's batch as much as possible.
-        """
-
-        for viewsb_packet in viewsb_packets:
-            top_level_item = QtWidgets.QTreeWidgetItem(get_packet_string_array(viewsb_packet))
-            top_level_item.setData(0, QtCore.Qt.UserRole, viewsb_packet)
-
-            list_of_children = []
-            recursive_packet_walk(viewsb_packet, list_of_children)
-
-            top_level_item.addChildren(list_of_children)
-
-            self.window.usb_tree_widget.addTopLevelItem(top_level_item)
-
-    def tree_current_item_changed(self, current_item, previous_item):
-        """ Use the side panel to show a detailed view of the current item. """
-
-        # Clear the details widget.
-        self.window.usb_details_tree_widget.clear()
-
-        self.window.usb_details_tree_widget.setColumnCount(2)
-
-        current_packet: ViewSBPacket = current_item.data(0, QtCore.Qt.UserRole)
-
-        # A list of 2-tuples: first element is a table title, and the second is usually a string: string dict
-        detail_fields = current_packet.get_detail_fields()
+    def _update_detail_fields(self, detail_fields):
 
         # Each table will have a root item in the details view.
         root_items = []
 
         for table in detail_fields:
-            title: str = table[0]
+            title = table[0]
 
             root = QtWidgets.QTreeWidgetItem([title])
             children = []
 
             fields = table[1]
 
-            # The usual case: a str: str dict.
-            if type(fields) == type({}):
+            # The usual case: a str:str dict.
+            if isinstance(fields, dict):
                 for key, value in fields.items():
                     children.append(QtWidgets.QTreeWidgetItem(stringify_list([key, value])))
 
             # Sometimes a descriptor will just be a 1-column list.
-            elif type(fields) == type([]):
+            elif isinstance(fields, list):
                 for item in fields:
                     children.append(QtWidgets.QTreeWidgetItem([str(item)]))
 
@@ -194,6 +114,95 @@ class QtFrontend(ViewSBFrontend):
 
         self.window.usb_details_tree_widget.resizeColumnToContents(0)
         self.window.usb_details_tree_widget.resizeColumnToContents(1)
+
+
+    def __init__(self):
+        """ Sets up the Qt UI. """
+
+        QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
+
+        self.app = QApplication([])
+        self.ui_file = QtCore.QFile(os.path.dirname(os.path.realpath(__file__)) + '/qt.ui')
+
+        self.loader = QUiLoader()
+        self.window = self.loader.load(self.ui_file)
+
+        # The default column size is too small for the timestamp and summary columns.
+        self.window.usb_tree_widget.setColumnWidth(0, 120)
+        self.window.usb_tree_widget.setColumnWidth(1, 500)
+
+        self.window.update_timer = QtCore.QTimer()
+        self.window.update_timer.timeout.connect(self.update)
+
+        self.window.usb_tree_widget.currentItemChanged.connect(self.tree_current_item_changed)
+
+        self.window.usb_tree_widget = self.window.usb_tree_widget
+        self.window.usb_tree_widget.sortByColumn(0)
+
+
+        self.window.showMaximized()
+
+
+    def update(self):
+        """ Called by the QTimer `update_timer`, collects packets waiting the queue and adds them to the tree view.
+
+        Note: Since this is called via a QTimer signal, this method runs in the UI thread.
+        """
+
+        packet_list = []
+
+        try:
+
+            # Get as many packets as we can as quick as we can.
+            while(True):
+
+                packet = self.data_queue.get_nowait()
+                packet_list.append(packet)
+
+        # But the instant it's empty, don't wait for any more; just send them to be processed.
+        except multiprocessing.queues.Empty:
+            pass
+
+        finally:
+
+            # In case the queue was empty in the first place and didn't have anything ready.
+            if len(packet_list) > 0:
+
+                self.add_packets(packet_list)
+
+
+    def add_packets(self, viewsb_packets):
+        """ Adds a list of top-level ViewSB packets to the tree.
+
+        We're in the UI thread; every bit of overhead counts, so let's batch as much as possible.
+        """
+
+        for viewsb_packet in viewsb_packets:
+            top_level_item = QtWidgets.QTreeWidgetItem(get_packet_string_array(viewsb_packet))
+            top_level_item.setData(0, QtCore.Qt.UserRole, viewsb_packet)
+
+            list_of_children = []
+            recursive_packet_walk(viewsb_packet, list_of_children)
+
+            top_level_item.addChildren(list_of_children)
+
+            self.window.usb_tree_widget.addTopLevelItem(top_level_item)
+
+
+    def tree_current_item_changed(self, current_item, previous_item):
+        """ Use the side panel to show a detailed view of the current item. """
+
+        # Clear the details widget.
+        self.window.usb_details_tree_widget.clear()
+
+        current_packet = current_item.data(0, QtCore.Qt.UserRole)
+
+        # A list of 2-tuples: first element is a table title, and the second is usually a string:string dict
+        detail_fields = current_packet.get_detail_fields()
+
+        if detail_fields:
+            self._update_detail_fields(detail_fields)
+
 
     def run(self):
         """ Overrides `ViewSBFrontend.run()` """

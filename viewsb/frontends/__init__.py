50ffe2f5f925219a9d07d7217d30ce03499fafd6
Filipe Laíns <lains@archlinux.org>
2020-09-22 01:23:59 +0100

    frontends: allow frontends to be a directory module
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- viewsb/frontends/__init__.py
+++ viewsb/frontends/__init__.py
@@ -1,7 +1,12 @@
-from os.path import dirname, basename, isfile
+from os.path import dirname, basename, isfile, join
 import glob
 
 # Autodetect all files in this directory, so they can be automatically
 # imported by 'from <module> import *'.
 modules = glob.glob(dirname(__file__)+"/*.py")
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f)]
+dir_modules = glob.glob(join(dirname(__file__), '*', '__init__.py'))
+__all__ = [
+    basename(f)[:-3] for f in modules if isfile(f)
+] + [
+    basename(dirname(f)) for f in dir_modules if isfile(f)
+]

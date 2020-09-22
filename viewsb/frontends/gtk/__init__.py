07dc5a749639b96f9d3ae2ecd980c4cac305207c
Filipe Laíns <lains@archlinux.org>
2020-09-22 21:46:30 +0100

    frontends/gtk: add initial hexview implementation
    In the future I want to have a more advanced hexview widget.
    
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- viewsb/frontends/gtk/__init__.py
+++ viewsb/frontends/gtk/__init__.py
@@ -7,7 +7,6 @@ This file is part of ViewSB
 
 import os.path
 import threading
-import time
 
 try:
     import gi
@@ -27,7 +26,7 @@ class GtkFrontend(ViewSBFrontend):
     UI_NAME = 'gtk'
     UI_DESCRIPTION = 'unstable GUI in GTK'
 
-    _COLUMNS = {
+    _DATA = {
         'Timestamp': GObject.TYPE_ULONG,
         'Device': GObject.TYPE_UINT,
         'Endpoint': GObject.TYPE_UINT,
@@ -36,16 +35,23 @@ class GtkFrontend(ViewSBFrontend):
         'Summary': str,
         'Status': str,
         'Data': str,
+        '_id': GObject.TYPE_ULONG,
     }
 
     class _GtkHandler:
-        def __init__(self, app_run_event=None):
-            self.app_run_event = app_run_event
+        def __init__(self, parent):
+            self.parent = parent
 
         def on_application_exit(self, *args):
             Gtk.main_quit()
-            if self.app_run_event:
-                self.app_run_event.clear()
+            if self.parent._app_run_event:
+                self.parent._app_run_event.clear()
+
+        def on_packet_cursor_changed(self, treeview):
+            model, treeiter = treeview.get_selection().get_selected()
+            if treeiter is not None:
+                packet = self.parent._packets[model[treeiter][8]]
+                self.parent._label_hexview.set_text(' '.join(f'{b:02x}' for b in packet.data))
 
         def on_scrolledwindowdata_size_allocate(self, widget, *args):
             # autoscroll
@@ -69,6 +75,7 @@ class GtkFrontend(ViewSBFrontend):
 
     def __init__(self):
         self._app_run_event = threading.Event()
+        self._packets = []
 
         self._ui = {}
         for ui in ('MainWindow',):
@@ -77,14 +84,17 @@ class GtkFrontend(ViewSBFrontend):
         self._builder = Gtk.Builder()
         self._builder.add_from_file(self._ui['MainWindow'])
 
-        self._builder.connect_signals(self._GtkHandler(self._app_run_event))
+        self._builder.connect_signals(self._GtkHandler(self))
 
         self._scrolledwindowData = self._builder.get_object('scrolledwindowData')
 
         self._treeview_data = self._builder.get_object('treeviewData')
-        self._liststore_data = Gtk.ListStore(*self._COLUMNS.values())
+        self._liststore_data = Gtk.ListStore(*self._DATA.values())
         self._treeview_data.set_model(self._liststore_data)
         self._init_treeview_data_columns()
+
+        self._panned_data_details = self._builder.get_object('pannedDataDetails')
+        self._label_hexview = self._builder.get_object('labelHexview')
 
         self._window = self._builder.get_object('window')
         self._window.show_all()
@@ -92,12 +102,15 @@ class GtkFrontend(ViewSBFrontend):
         self._gtk_thread = threading.Thread(target=Gtk.main)
 
     def _init_treeview_data_columns(self):
-        for i, column in enumerate(self._COLUMNS.keys()):
+        for i, column in enumerate(self._DATA.keys()):
+            if column.startswith('_'):
+                continue
             cell = Gtk.CellRendererText()
             col = Gtk.TreeViewColumn(column, cell, text=i)
             self._treeview_data.append_column(col)
 
     def handle_incoming_packet(self, packet):
+        self._packets.append(packet)
         self._liststore_data.append((
             packet.timestamp,
             packet.device_address,
@@ -106,7 +119,8 @@ class GtkFrontend(ViewSBFrontend):
             len(packet.data),
             packet.summarize(),
             packet.summarize_status(),
-            ' '.join(f'{b:02x}' for b in packet.data),
+            packet.summarize_data() or '',
+            len(self._packets) - 1,
         ))
 
     def run(self):
